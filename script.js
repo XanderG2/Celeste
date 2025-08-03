fbb4dd80fd5e94d870463dd7b033ee9f5a941115
Xander <XanderG2@users.noreply.github.com>
2025-08-03 22:04:38 +0100

    Tells user stuff idk

--- script.js
+++ script.js
@@ -4,7 +4,45 @@ function fileSubmit() {
   const reader = new FileReader();
   reader.onload = function () {
     const contents = reader.result;
-    console.log(contents);
+    handle(contents);
   };
   reader.readAsText(file);
+}
+
+function handle(contents) {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(contents, "text/xml");
+  const importantInfo = [
+    "Name",
+    "Time",
+    "TotalDeaths",
+    "TotalStrawberries",
+    "TotalGoldenStrawberries",
+    "TotalJumps",
+    "TotalWallJumps",
+    "TotalDashes",
+  ];
+  let JSON = {};
+  for (id of importantInfo) {
+    JSON[id] = xmlDoc.getElementsByTagName(id)[0].childNodes[0].nodeValue;
+  }
+  console.log(JSON);
+  document.getElementById("output").innerHTML = `Stats: ${[
+    "Name",
+    JSON.Name,
+    "Time",
+    JSON.Time,
+    "TotalDeaths",
+    JSON.TotalDeaths,
+    "TotalStrawberries",
+    JSON.TotalStrawberries,
+    "TotalGoldenStrawberries",
+    JSON.TotalGoldenStrawberries,
+    "TotalJumps",
+    JSON.TotalJumps,
+    "TotalWallJumps",
+    JSON.TotalWallJumps,
+    "TotalDashes",
+    JSON.TotalDashes,
+  ]}`;
 }
