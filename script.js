0a65612e588db30911c63a94610d9aaa7099b0a7
Xander <XanderG2@users.noreply.github.com>
2025-08-04 22:39:11 +0100

    Display better on website, make repo public

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function fileSubmit() {
   const reader = new FileReader();
   reader.onload = function () {
     const contents = reader.result;
-    handle(contents);
+    const KVP = handle(contents);
+    pretty(KVP);
   };
   reader.readAsText(file);
 }
@@ -21,13 +22,7 @@ function handle(contents) {
   for (let i = 0; i < K.length; i++) {
     KVP.push([K[i], V[i]]);
   }
-  console.log(KVP);
-  console.log(K, V);
-  document.getElementById("output").innerHTML = `Stats: ${JSON.stringify(
-    JSoN,
-    null,
-    2
-  )}`;
+  return KVP;
 }
 
 function BaseStats(xmlDoc) {
@@ -106,6 +101,43 @@ function AreaStats(xmlDoc) {
       C: CsideStats,
     };
   }
-  console.log(JSON);
   return JSON;
+}
+
+function pretty(KVP) {
+  const outputDiv = document.getElementById("output");
+  let KVP2 = [];
+  for (pair of KVP) {
+    const div = document.createElement("div");
+    div.id = pair[0];
+    if (typeof pair[1] === "object") {
+      KVP2.push(pair);
+    } else {
+      div.innerHTML = `<h1>${pair[0]}</h1> <p>${pair[1]}</p>`;
+    }
+    outputDiv.appendChild(div);
+  }
+  for (pair of KVP2) {
+    const div = document.createElement("div");
+    div.id = pair[0];
+    div.innerHTML = `<h1>${pair[0]}</h1>`;
+    console.log(pair[1]);
+    for (const [K, V] of Object.entries(pair[1])) {
+      const div2 = document.createElement("div");
+      div2.id = K;
+      div2.innerHTML = `<h2>${K}</h2>`;
+      if (typeof V === "object") {
+        for (const [K2, V2] of Object.entries(V)) {
+          const div3 = document.createElement("div");
+          div3.id = K2;
+          div3.innerHTML = `<h3>${K2}</h3> <p>${V2}</p>`;
+          div2.appendChild(div3);
+        }
+      } else {
+        div2.innerHTML += `<p>${V}</p>`;
+      }
+      div.appendChild(div2);
+    }
+    outputDiv.appendChild(div);
+  }
 }
